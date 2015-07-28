04b6c55fe031d87027ba0201a809aa471ce4f8fb
Chris MacLellan <maclellan.christopher@gmail.com>
2015-07-28 19:19:05 -0400

    updated files

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='py-search',
-    version='0.1.1',
+    version='0.1.0',
     author='Christopher J. MacLellan',
     author_email='maclellan.christopher@gmail.com',
     packages=setuptools.find_packages(),
     include_package_data = True,
     url='http://pypi.python.org/pypi/py-search/',
     license='LICENSE.txt',
-    description='A library of various graph search algorithms.',
+    description='A library of graph search algorithms.',
     long_description=open('README.rst').read(),
     install_requires=[],
 )
