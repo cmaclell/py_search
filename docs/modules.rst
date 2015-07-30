97f503d9a385b757282634c8f9e4ec09585d7663
Chris MacLellan <maclellan.christopher@gmail.com>
2015-07-30 11:58:20 -0400

    updated version number and pushing to pip

--- docs/modules.rst
+++
@@ -1,7 +0,0 @@
-py_search
-=========
-
-.. toctree::
-   :maxdepth: 4
-
-   py_search
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='py_search',
-    version='0.1.1',
+    version='0.1.2',
     author='Christopher J. MacLellan',
     author_email='maclellan.christopher@gmail.com',
     packages=setuptools.find_packages(),
     include_package_data = True,
     url='http://pypi.python.org/pypi/py_search/',
     license='LICENSE.txt',
-    description='A library of graph search algorithms.',
+    description='A library of search algorithms.',
     long_description=open('README.rst').read(),
     install_requires=['tabulate'],
 )
