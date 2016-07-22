0d4b2cf63767c57de0c0d8358d12c2aa7b918595
Chris MacLellan <maclellan.christopher@gmail.com>
2016-07-22 12:40:30 -0400

    updated version info for pushing to pip

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ author = 'Christopher J. MacLellan'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0'
+version = '1.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='py_search',
-    version='1.0.0',
+    version='1.0.1',
     author='Christopher J. MacLellan',
     author_email='maclellan.christopher@gmail.com',
     packages=setuptools.find_packages(),
