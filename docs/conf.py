f76fb59a79f865fcf5ce3326ebfcca069d642bea
Chris MacLellan <maclellan.christopher@gmail.com>
2016-07-29 10:50:37 -0400

    upping pip version

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ author = 'Christopher J. MacLellan'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.2'
+version = '1.0.3'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='py_search',
-    version='1.0.2',
+    version='1.0.3',
     author='Christopher J. MacLellan',
     author_email='maclellan.christopher@gmail.com',
     packages=setuptools.find_packages(),
