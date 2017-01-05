aeed3b3a0a5c1cce57dfcab8bb5a67b489b6f0ab
Chris MacLellan <maclellan.christopher@gmail.com>
2017-01-05 10:52:27 -0500

    added runtime to compare function

--- py_search/utils.py
+++ py_search/utils.py
@@ -8,17 +8,19 @@ from __future__ import absolute_import
 from __future__ import division
 
 from tabulate import tabulate
+import timeit
 
 from py_search.base import AnnotatedProblem
+
 
 def compare_searches(problems, searches):
     """
     A function for comparing different search algorithms on different problems.
 
     :param problems: problems to solve.
-    :type problems: typically a list of problems, but could be an iterator
+    :type problems: an iterator of problems (usually a list)
     :param searches: search algorithms to use.
-    :type searches: typically a list of search functions, but could be an iterator
+    :type searches: an iterator of search functions (usually a list)
     """
     table = []
 
@@ -27,18 +29,22 @@ def compare_searches(problems, searches):
             annotated_problem = AnnotatedProblem(problem)
 
             try:
+                start_time = timeit.default_timer()
                 sol = next(search(annotated_problem))
+                elapsed = timeit.default_timer() - start_time
                 value = problem.node_value(sol)
             except StopIteration:
                 value = 'Failed'
+                elapsed = 'Failed'
 
             table.append([problem.__class__.__name__, search.__name__,
                           annotated_problem.goal_tests,
                           annotated_problem.nodes_expanded,
                           annotated_problem.nodes_evaluated, "%0.3f" % value if
-                          isinstance(value, float) else value])
+                          isinstance(value, float) else value,
+                          "%0.4f" % elapsed])
 
     print(tabulate(table, headers=['Problem', 'Search Alg', 'Goal Tests',
                                    'Nodes Expanded', 'Nodes Evaluated',
-                                   'Solution Cost'],
+                                   'Solution Cost', 'Runtime'],
                    tablefmt="simple"))
