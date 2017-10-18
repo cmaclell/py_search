c0e61debbc4ef0992333e2609fb716b5cdad7979
Chris MacLellan <chris.maclellan@soartech.com>
2017-10-18 10:06:26 -0400

    cleaned up indenting

--- py_search/informed.py
+++ py_search/informed.py
@@ -1,7 +1,7 @@
 """
 This module includes the informed search techniques: :func:`best_first_search`
 (i.e., A*), :func:`iterative_deepening_best_first_search` (i.e., IDA*),
-:func:`beam_search`, and :func:`widening_beam_search`. 
+:func:`beam_search`, and :func:`widening_beam_search`.
 """
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -10,7 +10,7 @@ from __future__ import division
 
 from py_search.base import PriorityQueue
 from py_search.uninformed import graph_search
-from py_search.uninformed import tree_search
+
 
 def best_first_search(problem, search=graph_search, cost_limit=float('inf')):
     """
@@ -20,8 +20,8 @@ def best_first_search(problem, search=graph_search, cost_limit=float('inf')):
     to maintain the fringe.
 
     If the problem.node_value is the cost to reach the node plus an admissible
-    heuristic estimate of the distance from the node to the goal, then this is the
-    A* algorithm. 
+    heuristic estimate of the distance from the node to the goal, then this is
+    the A* algorithm.
 
     :param problem: The problem to solve.
     :type problem: :class:`Problem`
@@ -30,13 +30,15 @@ def best_first_search(problem, search=graph_search, cost_limit=float('inf')):
     :param cost_limit: The cost limit for the search (default = `float('inf')`)
     :type cost_limit: float
     """
-    for solution in search(problem, PriorityQueue(node_value=problem.node_value,
-                                                  cost_limit=cost_limit)):
+    for solution in search(problem,
+                           PriorityQueue(node_value=problem.node_value,
+                                         cost_limit=cost_limit)):
         yield solution
+
 
 def iterative_deepening_best_first_search(problem, search=graph_search,
                                           initial_cost_limit=0, cost_inc=1,
-                                          max_cost_limit=float('inf')): 
+                                          max_cost_limit=float('inf')):
     """
     A variant of iterative deepening that uses cost to determine the limit for
     expansion. When search fails, the cost limit is increased according to
@@ -44,8 +46,8 @@ def iterative_deepening_best_first_search(problem, search=graph_search,
     the best solution (similar to best first serach), but uses less memory.
 
     If the problem.node_value is the cost to reach the node plus an admissible
-    heuristic estimate of the distance from the node to the goal, then this is the
-    IDA* algorithm. 
+    heuristic estimate of the distance from the node to the goal, then this is
+    the IDA* algorithm.
 
     :param problem: The problem to solve.
     :type problem: :class:`Problem`
@@ -55,22 +57,26 @@ def iterative_deepening_best_first_search(problem, search=graph_search,
     :type initial_cost_limit: float
     :param cost_inc: The amount to increase the cost limit after failure.
     :type cost_inc: float
-    :param max_cost_limit: The maximum cost limit (default value of `float('inf')`)
+    :param max_cost_limit: The maximum cost limit (default value of
+        `float('inf')`)
     :type max_cost_limit: float
     """
     cost_limit = initial_cost_limit
     while cost_limit < max_cost_limit:
-        for solution in search(problem, PriorityQueue(cost_limit=cost_limit,
-                                                      node_value=problem.node_value)):
+        for solution in search(problem,
+                               PriorityQueue(cost_limit=cost_limit,
+                                             node_value=problem.node_value)):
             yield solution
         cost_limit += cost_inc
+
 
 def beam_search(problem, beam_width=1, graph_search=True):
     """
     A variant of breadth-first search where all nodes in the fringe
     are expanded, but the resulting new fringe is limited to have length
     beam_width, where the nodes with the worst value are dropped. The default
-    beam width is 1, which yields greedy best-first search (i.e., hill climbing).
+    beam width is 1, which yields greedy best-first search (i.e., hill
+    climbing).
 
     There are different ways to implement beam search, namely best-first
     beam search and breadth-first beam search. According to:
@@ -113,16 +119,17 @@ def beam_search(problem, beam_width=1, graph_search=True):
                     fringe.push(s)
                     closed[s] = s.cost()
 
+
 def widening_beam_search(problem, initial_beam_width=1,
-                               max_beam_width=1000, graph_search=True):
+                         max_beam_width=1000, graph_search=True):
     """
     A variant of beam search that successively increase the beam width when
-    search fails. This ensures that if a solution exists, and you're using graph
-    search, and the solution space is finite, then that beam search will
-    find it. 
+    search fails. This ensures that if a solution exists, and you're using
+    graph search, and the solution space is finite, then that beam search will
+    find it.
 
-    However, if you are looking for multiple solutions, then it might
-    return duplicates as the width is increased. As the beam width increase, behavior
+    However, if you are looking for multiple solutions, then it might return
+    duplicates as the width is increased. As the beam width increase, behavior
     is closer and closer to breadth-first search.
 
     :param problem: The problem to solve.
@@ -134,6 +141,7 @@ def widening_beam_search(problem, initial_beam_width=1,
     """
     beam_width = initial_beam_width
     while beam_width <= max_beam_width:
-        for solution in beam_search(problem, beam_width=beam_width, graph_search=graph_search):
+        for solution in beam_search(problem, beam_width=beam_width,
+                                    graph_search=graph_search):
             yield solution
         beam_width += 1
