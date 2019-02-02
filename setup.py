222cc0baf04af1dbcf45ff42c416054eef37ce59
cmaclell <maclellan.christopher@gmail.com>
2019-02-01 20:55:45 -0500

    refactoring packaging, added testing for travis.

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
-    name='py_search',
-    version='2.0.1',
-    author='Christopher J. MacLellan',
-    author_email='maclellan.christopher@gmail.com',
-    packages=setuptools.find_packages(),
-    include_package_data=True,
-    url='https://pypi.python.org/pypi/py_search/',
-    license='MIT License',
-    description=('A library of uninformed, informed, '
-                 'and optimization search algorithms'),
-    long_description=open('README.rst').read(),
-    install_requires=['tabulate', 'munkres'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    classifiers=['Development Status :: 4 - Beta',
-                 'Intended Audience :: Science/Research',
-                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
-                 'License :: OSI Approved :: MIT License',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: Implementation :: PyPy'],
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
 )
